07d091c021420fb4c8566b826dec2465916f2d1f
sebastianLujan <glujan.recalde@gmail.com>
2019-11-04 19:09:33 -0300

    feature: mobile menu, and some fixes.
    chore: load page function and event listener
    
    feature: close button
    feature: vertical menu
    
    fix: styles and structure

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/* We use js functions and emojis just for fun */
+
+(
+  function loadPage() {
+    setTimeout( () => {
+      let list = document.querySelectorAll('img');
+      list.forEach( img => img.classList.add('load-in') )
+    } , 500);
+  }
+)();
+
+    let yummy = document.getElementById('🍔');
+
+    yummy.addEventListener('click', () => {
+
+    let laCarte = document.getElementById('📜');
+
+    if( yummy.classList.contains('x')) {
+      yummy.classList.remove('x');
+      laCarte.classList.remove('in');
+      laCarte.classList.add('out')
+    } else {
+      yummy.classList.add('x');
+      laCarte.classList.add('in');
+      laCarte.classList.add('vertical-menu');
+    }
+
+    /* removes the vertical-menu and out class in order reset the initial state */
+    if( laCarte.classList.contains('vertical-menu') && laCarte.classList.contains('out')) {
+      laCarte.classList.remove('out');
+      laCarte.classList.remove('vertical-menu');
+    }
+
+  });
+
+
+
+
+
+
